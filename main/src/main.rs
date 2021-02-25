95ed4bcf9b35fde8abbba3b9828d66e5ea1758df
antlilja <liljaanton2001@gmail.com>
2021-02-25 22:23:09 +0100

    Add

--- main/src/main.rs
+++ main/src/main.rs
@@ -48,30 +48,45 @@ fn main() {
     }
 
     let mut gloo = Vec::with_capacity(i); // Light schedule
+    let mut real_len = 0;
+
     for id in 0..i {
         let mut streets = Vec::with_capacity(bloo[id].len());
+
+        let mut total = 0usize;
+
         for n in bloo[id].iter() {
-            if floo.contains_key(n) {
-                streets.push((*n, 1usize));
+            if let Some(a) = floo.get(n) {
+                total += a;
+            }
+        }
+
+        for n in bloo[id].iter() {
+            if let Some(a) = floo.get(n) {
+                let time = (((*a as f64) / (total as f64)) * (d as f64)) as usize;
+                streets.push((*n, time));
             }
         }
         if streets.len() != 0 {
             gloo.push(streets);
+            real_len += 1;
         } else {
             gloo.push(Vec::new());
         }
     }
 
-    let mut sim = Simulator::new(&streets, &cars, i, gloo.clone(), d, f);
+    // let mut sim = Simulator::new(&streets, &cars, i, gloo.clone(), d, f);
 
-    println!("Score: {}", sim.run());
+    // println!("Score: {}", sim.run());
 
-    // println!("{}", gloo.len());
-    // for g in gloo {
-    //     println!("{}", g.0);
-    //     println!("{}", g.1.len());
-    //     for n in g.1 {
-    //         println!("{} 1", n);
-    //     }
-    // }
+    println!("{}", real_len);
+    for (i, g) in gloo.iter().enumerate() {
+        if !g.is_empty() {
+            println!("{}", i);
+            println!("{}", g.len());
+            for i in g {
+                println!("{} {}", i.0, i.1);
+            }
+        }
+    }
 }
